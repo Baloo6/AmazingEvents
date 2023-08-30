a8321a32c0909c207f8776a54ff7493d9406d2c3
Baloo <iandiaz2001@gmail.com>
2023-08-29 21:52:36 -0300

    optimizando con funciones los scripts

--- scripts/past_events.js
+++ scripts/past_events.js
@@ -1,23 +1,38 @@
-let template= ''
+
+let arrayPasado= []
 let today = Date.now()
-
-
-for(let event of data.events){
-    if(Date.parse(data.currentDate) > Date.parse(event.date)){
-        template += `<div class="card" style="width: 18rem;">
-        <img src="${event.image}" class="card-img-top" alt="event cinema">
-        <div class="card-body">
-        <h5 class="card-title">${event.name}</h5>
-        <p class="card-text">${event.description}</p>
-        <div class="detail-btn">
-            <p>Price:${event.price}</p>
-            <a href="details.html" class="btn btn-detail">Details</a>
-        </div> 
-        </div>
-    </div>`
+let dataEvents = data.events
+// Funcion de Filtrado past
+function filtrarEventos(parametroArray){
+    for(let event of parametroArray){
+        if(Date.parse(data.currentDate) > Date.parse(event.date)){
+            arrayPasado.push(event)
+        }
     }
 }
+filtrarEventos(dataEvents)
+
+// Funcion para agregar info
+function addInfo(arrayFiltrado){
+    let template= ''
+        for(let event of arrayFiltrado){
+            
+            template += `<div class="card" style="width: 18rem;">
+            <img src="${event.image}" class="card-img-top" alt="event cinema">
+            <div class="card-body">
+            <h5 class="card-title">${event.name}</h5>
+            <p class="card-text">${event.description}</p>
+            <div class="detail-btn">
+                <p>Price:${event.price}</p>
+                <a href="details.html" class="btn btn-detail">Details</a>
+            </div> 
+            </div>
+        </div>`
+    } 
+    return template
+ 
+}
+let estructuraCard = addInfo(arrayPasado)
 
 let cardsDinamicas = document.getElementById('cards_div')
-
-cardsDinamicas.innerHTML += template
+cardsDinamicas.innerHTML += estructuraCard
