5dbf05b4b67afb1914d16af26d04a31f51cc3244
Baloo <iandiaz2001@gmail.com>
2023-08-31 22:02:13 -0300

    fav animation fixed

--- scripts/index.js
+++ scripts/index.js
@@ -2,7 +2,8 @@ let template= ''
 let cardContador= 0;
 for(let event of data.events){
     cardContador++ ;
-    template += `<div class="card _fondo" style="width: 18rem;"> <i class="favorite btn position-absolute top-0 end-0 bi bi-heart-fill" id="fav"></i>
+    template += `<div class="card _fondo" style="width: 18rem;">
+    <i class="favorite btn position-absolute top-0 end-0 bi bi-heart-fill" id="like"></i>
     <img src="${event.image}" class="card-img-top" alt="event cinema">
     <div class="card-body">
       <h5 class="card-title">${event.name}</h5>
@@ -15,17 +16,17 @@ for(let event of data.events){
 </div>`
 }
 
-// Función para marcar eventos favoritos
-const favButtons = document.querySelectorAll('.favorite');
-favButtons.forEach(function (button) {
-    button.addEventListener('click', function () {
-        this.classList.toggle('like');
-    });
-});
-
 let cardsDinamicas = document.getElementById('cards_div')
 cardsDinamicas.innerHTML += template
 
 let contadorCards = `<h3> Total events: ${cardContador} </h3>`
 let categoriesDiv = document.getElementById('categories_div')
 categoriesDiv.innerHTML += contadorCards
+
+// Función para marcar eventos favoritos
+let favButtons = document.querySelectorAll('.favorite');
+favButtons.forEach(function (button) {
+      button.addEventListener('click', function () {
+          this.classList.toggle('like');
+      });
+  });
