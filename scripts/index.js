0fd55454642ac5667d6c0ca064709b86697e5f8b
Baloo <iandiaz2001@gmail.com>
2023-09-09 11:29:01 -0300

    fetch added

--- scripts/index.js
+++ scripts/index.js
@@ -1,105 +1,121 @@
-const arrayData = data.events
 const $contenedorChecks = document.getElementById('catContainer')
 const $containerCards = document.getElementById('cards_div')
 
-// Creamos la categorias sin repetir para los checks
-const cat_array = [...new Set(arrayData.map(objeto => objeto.category))];
+async function fetchData() {
 
-// Crear checks
-function crearChecks(string) {
-     let template = ""
-     return template = `
-     <label class="me-2">
-          <input type="checkbox" class="me-2" name="check" value="${string}">${string}
-     </label>
-     `
+    try {
+        const respuesta = await fetch('https://mindhub-xj03.onrender.com/api/amazing');
+        const data = await respuesta.json();
+        final(data);
+    }
+     
+    catch {
+        console.error('Error in try fetching');
+    }
 }
+fetchData()
 
-// Imprimir checks
-function imprimirChecks(array, elementoHTML) {
-     let estructura = ""
-     array.forEach(string => {
-          estructura += crearChecks(string)
-     })
-     elementoHTML.innerHTML += estructura
-}
-imprimirChecks(cat_array, $contenedorChecks)
-
-
-
-// Crear Cards
-function crearCard(arrayData) {
-     let template = ""
-     return template = `
-        <div class="card _fondo" style="width: 18rem;">
-            <i class="favorite btn position-absolute top-0 end-0 bi bi-heart-fill" id="like"></i>
-            <img src="${arrayData.image}" class="card-img-top" alt="event cinema">
-            <div class="card-body">
-                  <h5 class="card-title">${arrayData.name}</h5>
-                  <p class="card-text">${arrayData.description}</p>
-            </div>
-            <div class="detail-btn">
-                    <p>Price:${arrayData.price}</p>
-                    <a href="details.html?id=${arrayData._id}" class="btn btn-detail">Details</a>
-            </div> 
-            
-        </div>
-     `
-}
-
-// Fn imprimir cards
-function imprimirCards(array, elementoHTML) {
-          let estructura = ""
-          array.forEach(string => {
-               estructura += crearCard(string)
-          })
-          elementoHTML.innerHTML = estructura
-}
-imprimirCards(arrayData, $containerCards)
-
-// Función para marcar eventos favoritos
-let favButtons = document.querySelectorAll('.favorite');
-favButtons.forEach(function (button) {
-      button.addEventListener('click', function () {
-          this.classList.toggle('like');
-      });
-  });
-  
-
-// Checks
-$contenedorChecks.addEventListener("change", (e) => {
-    const return_filtros = fn_filtros(arrayData, $search)
-    imprimirCards(return_filtros, $containerCards)
-})
-
-function filtroChecks(array){
-    let arrayValues = Array.from(document.querySelectorAll('input[type="checkbox"]:checked')).map(check => check.value)
-    if (arrayValues.length != 0){
-        let arrayChecks = array.filter(objeto => arrayValues.includes(objeto.category))
-        return arrayChecks
-    } else {return arrayData}
+const final = (data)  => {
+    const arrayData = data.events
     
-}
+
+        // Creamos la categorias sin repetir para los checks
+        const cat_array = [...new Set(arrayData.map(objeto => objeto.category))]
+
+        // Crear checks
+        function crearChecks(string) {
+            let template = ""
+            return template = `
+            <label class="me-2">
+                <input type="checkbox" class="me-2" name="check" value="${string}">${string}
+            </label>
+            `
+        }
+
+        // Imprimir checks
+        function imprimirChecks(array, elementoHTML) {
+            let estructura = ""
+            array.forEach(string => {
+                estructura += crearChecks(string)
+            })
+            elementoHTML.innerHTML += estructura
+        }
+        imprimirChecks(cat_array, $contenedorChecks)
 
 
-//Search
-const $search = document.querySelector('input[type="search"]')
 
-$search.addEventListener( "keyup", ()=>{
-    const return_filtros = fn_filtros(arrayData, $search)
-    imprimirCards(return_filtros, $containerCards)
-})
+        // Crear Cards
+        function crearCard(arrayData) {
+            let template = ""
+            return template = `
+                <div class="card _fondo" style="width: 18rem;">
+                    <i class="favorite btn position-absolute top-0 end-0 bi bi-heart-fill" id="like"></i>
+                    <img src="${arrayData.image}" class="card-img-top" alt="event cinema">
+                    <div class="card-body">
+                        <h5 class="card-title">${arrayData.name}</h5>
+                        <p class="card-text">${arrayData.description}</p>
+                    </div>
+                    <div class="detail-btn">
+                            <p>Price:${arrayData.price}</p>
+                            <a href="details.html?id=${arrayData._id}" class="btn btn-detail">Details</a>
+                    </div> 
+                    
+                </div>
+            `
+        }
 
-function filtroSearch(array, input){
-    let arraySearch = array.filter(objeto => objeto.name.toLowerCase().includes(input.value.toLowerCase()))
-    console.log(arraySearch);
-    return arraySearch
-}
+        // Fn imprimir cards
+        function imprimirCards(array, elementoHTML) {
+                let estructura = ""
+                array.forEach(string => {
+                    estructura += crearCard(string)
+                })
+                elementoHTML.innerHTML = estructura
+        }
+        imprimirCards(arrayData, $containerCards)
 
-//Juntar
-function fn_filtros(array, input){
-    const af_check = filtroChecks(array)
-    const af_search = filtroSearch(af_check, input)
-    return af_search
-}
+        // Función para marcar eventos favoritos
+        let favButtons = document.querySelectorAll('.favorite');
+        favButtons.forEach(function (button) {
+            button.addEventListener('click', function () {
+                this.classList.toggle('like');
+            });
+        });
+        
 
+        // Checks
+        $contenedorChecks.addEventListener("change", (e) => {
+            const return_filtros = fn_filtros(arrayData, $search)
+            imprimirCards(return_filtros, $containerCards)
+        })
+
+        function filtroChecks(array){
+            let arrayValues = Array.from(document.querySelectorAll('input[type="checkbox"]:checked')).map(check => check.value)
+            if (arrayValues.length != 0){
+                let arrayChecks = array.filter(objeto => arrayValues.includes(objeto.category))
+                return arrayChecks
+            } else {return arrayData}
+            
+        }
+
+
+        //Search
+        const $search = document.querySelector('input[type="search"]')
+
+        $search.addEventListener( "keyup", ()=>{
+            const return_filtros = fn_filtros(arrayData, $search)
+            imprimirCards(return_filtros, $containerCards)
+        })
+
+        function filtroSearch(array, input){
+            let arraySearch = array.filter(objeto => objeto.name.toLowerCase().includes(input.value.toLowerCase()))
+            return arraySearch
+        }
+
+        //Juntar
+        function fn_filtros(array, input){
+            const af_check = filtroChecks(array)
+            const af_search = filtroSearch(af_check, input)
+            return af_search
+        }
+    }
--- scripts/upc_events.js
+++ scripts/upc_events.js
@@ -1,117 +1,133 @@
-let arrayPasado= []
-let today = Date.now()
-let events = data.events
-const arrayData = data.events
 const $contenedorChecks = document.getElementById('catContainer')
 const $containerCards = document.getElementById('cards_div')
-// Funcion de Filtrado up
-function filtrarEventos(parametroArray){
-    for(let event of parametroArray){
-        if(Date.parse(data.currentDate) < Date.parse(event.date)){
-            arrayPasado.push(event)
-        }
+
+async function fetchData() {
+
+    try {
+        const respuesta = await fetch('https://mindhub-xj03.onrender.com/api/amazing');
+        const data = await respuesta.json();
+        final(data);
+    }
+     
+    catch {
+        console.error('Error in try fetching');
     }
 }
-filtrarEventos(events)
+fetchData()
+
+const final = (data)  => {
+    const arrayData = data.events
+    let arrayPasado= []
+
+        // Funcion de Filtrado up
+        function filtrarEventos(parametroArray){
+            for(let event of parametroArray){
+                if(Date.parse(data.currentDate) < Date.parse(event.date)){
+                    arrayPasado.push(event)
+                }
+            }
+        }
+        filtrarEventos(arrayData)
 
 
-// Creamos la categorias sin repetir para los checks
-const cat_array = [...new Set(arrayPasado.map(objeto => objeto.category))];
+        // Creamos la categorias sin repetir para los checks
+        const cat_array = [...new Set(arrayPasado.map(objeto => objeto.category))];
 
-// Crear checks
-function crearChecks(string) {
-     let template = ""
-     return template = `
-     <label class="me-2">
-          <input type="checkbox" class="me-2" name="check" value="${string}">${string}
-     </label>
-     `
-}
+        // Crear checks
+        function crearChecks(string) {
+            let template = ""
+            return template = `
+            <label class="me-2">
+                <input type="checkbox" class="me-2" name="check" value="${string}">${string}
+            </label>
+            `
+        }
 
-// Imprimir checks
-function imprimirChecks(array, elementoHTML) {
-     let estructura = ""
-     array.forEach(string => {
-          estructura += crearChecks(string)
-     })
-     elementoHTML.innerHTML += estructura
-}
-imprimirChecks(cat_array, $contenedorChecks)
+        // Imprimir checks
+        function imprimirChecks(array, elementoHTML) {
+            let estructura = ""
+            array.forEach(string => {
+                estructura += crearChecks(string)
+            })
+            elementoHTML.innerHTML += estructura
+        }
+        imprimirChecks(cat_array, $contenedorChecks)
 
 
 
-// Crear Cards
-function crearCard(arrayPasado) {
-     let template = ""
-     return template = `
-        <div class="card _fondo" style="width: 18rem;">
-            <i class="favorite btn position-absolute top-0 end-0 bi bi-heart-fill" id="like"></i>
-            <img src="${arrayPasado.image}" class="card-img-top" alt="event cinema">
-            <div class="card-body">
-                  <h5 class="card-title">${arrayPasado.name}</h5>
-                  <p class="card-text">${arrayPasado.description}</p>
-            </div>
-            <div class="detail-btn">
-                    <p>Price:${arrayPasado.price}</p>
-                    <a href="details.html?id=${arrayPasado._id}" class="btn btn-detail">Details</a>
-            </div> 
+        // Crear Cards
+        function crearCard(arrayPasado) {
+            let template = ""
+            return template = `
+                <div class="card _fondo" style="width: 18rem;">
+                    <i class="favorite btn position-absolute top-0 end-0 bi bi-heart-fill" id="like"></i>
+                    <img src="${arrayPasado.image}" class="card-img-top" alt="event cinema">
+                    <div class="card-body">
+                        <h5 class="card-title">${arrayPasado.name}</h5>
+                        <p class="card-text">${arrayPasado.description}</p>
+                    </div>
+                    <div class="detail-btn">
+                            <p>Price:${arrayPasado.price}</p>
+                            <a href="details.html?id=${arrayPasado._id}" class="btn btn-detail">Details</a>
+                    </div> 
+                    
+                </div>
+            `
+        }
+
+        // Fn imprimir cards
+        function imprimirCards(array, elementoHTML) {
+                let estructura = ""
+                array.forEach(string => {
+                    estructura += crearCard(string)
+                })
+                elementoHTML.innerHTML = estructura
+        }
+        imprimirCards(arrayPasado, $containerCards)
+
+        // Función para marcar eventos favoritos
+        let favButtons = document.querySelectorAll('.favorite');
+        favButtons.forEach(function (button) {
+            button.addEventListener('click', function () {
+                this.classList.toggle('like');
+            });
+        });
+        
+
+        // Checks
+        $contenedorChecks.addEventListener("change", (e) => {
+            const return_filtros = fn_filtros(arrayPasado, $search)
+            imprimirCards(return_filtros, $containerCards)
+        })
+
+        function filtroChecks(array){
+            let arrayValues = Array.from(document.querySelectorAll('input[type="checkbox"]:checked')).map(check => check.value)
+            if (arrayValues.length != 0){
+                let arrayChecks = array.filter(objeto => arrayValues.includes(objeto.category))
+                return arrayChecks
+            } else {return arrayPasado}
             
-        </div>
-     `
-}
-
-// Fn imprimir cards
-function imprimirCards(array, elementoHTML) {
-          let estructura = ""
-          array.forEach(string => {
-               estructura += crearCard(string)
-          })
-          elementoHTML.innerHTML = estructura
-}
-imprimirCards(arrayPasado, $containerCards)
-
-// Función para marcar eventos favoritos
-let favButtons = document.querySelectorAll('.favorite');
-favButtons.forEach(function (button) {
-      button.addEventListener('click', function () {
-          this.classList.toggle('like');
-      });
-  });
-  
-
-// Checks
-$contenedorChecks.addEventListener("change", (e) => {
-    const return_filtros = fn_filtros(arrayPasado, $search)
-    imprimirCards(return_filtros, $containerCards)
-})
-
-function filtroChecks(array){
-    let arrayValues = Array.from(document.querySelectorAll('input[type="checkbox"]:checked')).map(check => check.value)
-    if (arrayValues.length != 0){
-        let arrayChecks = array.filter(objeto => arrayValues.includes(objeto.category))
-        return arrayChecks
-    } else {return arrayPasado}
-    
-}
+        }
 
 
-//Search
-const $search = document.querySelector('input[type="search"]')
+        //Search
+        const $search = document.querySelector('input[type="search"]')
 
-$search.addEventListener( "keyup", ()=>{
-    const return_filtros = fn_filtros(arrayPasado, $search)
-    imprimirCards(return_filtros, $containerCards)
-})
+        $search.addEventListener( "keyup", ()=>{
+            const return_filtros = fn_filtros(arrayPasado, $search)
+            imprimirCards(return_filtros, $containerCards)
+        })
 
-function filtroSearch(array, input){
-    let arraySearch = array.filter(objeto => objeto.name.toLowerCase().includes(input.value.toLowerCase()))
-    console.log(arraySearch);
-    return arraySearch
-}
+        function filtroSearch(array, input){
+            let arraySearch = array.filter(objeto => objeto.name.toLowerCase().includes(input.value.toLowerCase()))
+            console.log(arraySearch);
+            return arraySearch
+        }
 
-//Juntar filtros
-function fn_filtros(array, input){
-    const af_check = filtroChecks(array)
-    const af_search = filtroSearch(af_check, input)
-    return af_search
+        //Juntar filtros
+        function fn_filtros(array, input){
+            const af_check = filtroChecks(array)
+            const af_search = filtroSearch(af_check, input)
+            return af_search
+        }
 }
